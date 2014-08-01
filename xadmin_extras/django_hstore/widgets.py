21e9f438e54c202379cd958430515bdde3db3866
Javier Cordero <jcorderomartinez@gmail.com>
2014-08-01 14:02:47 +0200

    Added django_hstore support

---
+++ xadmin_extras/django_hstore/widgets.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+from __future__ import unicode_literals, absolute_import
+
+from django import forms
+from django.contrib.admin.templatetags.admin_static import static
+
+from django_hstore.widgets import BaseAdminHStoreWidget
+
+
+class XAdminHStoreWidget(BaseAdminHStoreWidget):
+    admin_style = 'xadmin'
+
+    @property
+    def media(self):
+        # load underscore from CDNJS (popular javascript content delivery network)
+        external_js = [
+            "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"
+        ]
+
+        internal_js = [
+            "django_hstore/hstore-xadmin-widget.js"
+        ]
+
+        js = external_js + [static("xadmin/vendor/%s" % path) for path in internal_js]
+
+        return forms.Media(js=js)
