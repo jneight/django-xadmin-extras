d96658c65c3b6b1366f70dbf6ee86fd2d82b20e6
Javier Cordero <jcorderomartinez@gmail.com>
2014-08-05 13:28:56 +0200

    Preparing pypi package

---
+++ setup.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+import os
+from setuptools import setup, find_packages
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+import xadmin_extras as xae
+
+setup(
+    name='django-xadmin-extras',
+    version=xae.__version__,
+    url='https://github.com/jneight/django-xadmin-extras',
+    install_requires=['django-mail-factory'],
+    description="Extensions for django-xadmin",
+    author=xae.__author__,
+    author_email=xae.__email__,
+    packages=find_packages(exclude=['tests', 'tests.*',]),
+    include_package_data=True,
+    license=xae.__license__,
+    test_suite="tests",
+    zip_safe=False,
+)
---
+++ xadmin_extras/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+def get_version():
+    return '%s.%s' % (VERSION[0], VERSION[1])
+
+
+VERSION = (0, 10,)
+
+__version__ = get_version()
+__author__ = 'Javier Cordero'
+__email__ = 'jcorderomartinez@gmail.com'
+__license__ = 'Apache 2.0'
